c7b022f5134730059817f621486f6cd646639a82
jvdalagliounitri <joao.dalaglio@unitri.edu.br>
2025-03-20 10:58:44 -0300

    feat: Configurações dos gráficos por ano e bundle max

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,64 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { PoChartModule, PoChartOptions, PoChartSerie, PoChartType, PoDividerModule } from '@po-ui/ng-components';
+import { PoChartModule, PoChartOptions, PoChartSerie, PoChartType, PoDividerModule, PoFieldModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PoChartModule, PoDividerModule],
+  imports: [PoChartModule, PoDividerModule, PoFieldModule],
   templateUrl: './home.component.html',
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class HomeComponent {
-  evolutionOfCoffeeAndSomeCompetitors: Array<PoChartSerie> = [
-    { label: '2014', data: [91, 40, 42], type: PoChartType.Column },
-    { label: '2017', data: [93, 52, 18], type: PoChartType.Column },
-    { label: '2020', data: [95, 21, -17], type: PoChartType.Column },
-    { label: 'Coffee consumption in Brazil', data: [34, 27, 79], type: PoChartType.Line, color: 'color-10' }
+  public year: number = new Date().getFullYear();
+  vendasPorMes: Array<PoChartSerie> = [
+    { label: 'Janeiro', data: [3200], type: PoChartType.Column, color: 'color-07' },
+    { label: 'Fevereiro', data: [6400], type: PoChartType.Column, color: 'color-10' },
+    { label: 'Março', data: [5230], type: PoChartType.Column, color: 'color-07' },
+    { label: 'Vendas por mês', data: [3200, 6400, 5320], type: PoChartType.Line, color: 'color-10' }
   ];
-  categoriesColumn: Array<string> = ['coffee', 'chocolate', 'tea'];
+  categoriesColumn: Array<string> = ['Janeiro', 'Fevereiro', 'Março'];
   optionsColumn: PoChartOptions = {
     axis: {
-      minRange: -20,
-      maxRange: 100,
-      gridLines: 7
+      minRange: 0,
+      maxRange: 10000,
+      gridLines: 12
     }
   };
+
+  changeYear(event: number) {
+    this.year = event
+    if(this.year === 2024) {
+      this.vendasPorMes = [
+        { label: 'Janeiro', data: [3200], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Fevereiro', data: [2500], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Março', data: [7500], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Abril', data: [7200], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Maio', data: [6000], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Junho', data: [8000], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Julho', data: [8000], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Agosto', data: [6000], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Setembro', data: [3800], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Outubro', data: [6500], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Novembro', data: [7850], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Dezembro', data: [6200], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Vendas por mês', data: [3200, 2500, 7500, 7200, 6000, 8000, 8000, 6000, 3800, 6500, 7850, 6200], type: PoChartType.Line, color: 'color-10' }
+      ];
+      this.categoriesColumn = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    } else {
+      this.vendasPorMes = [
+        { label: 'Janeiro', data: [3200], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Fevereiro', data: [6400], type: PoChartType.Column, color: 'color-10' },
+        { label: 'Março', data: [5230], type: PoChartType.Column, color: 'color-07' },
+        { label: 'Vendas por mês', data: [3200, 6400, 5320], type: PoChartType.Line, color: 'color-10' }
+      ];
+      this.categoriesColumn = ['Janeiro', 'Fevereiro', 'Março'];
+      this.optionsColumn = {
+        axis: {
+          minRange: 0,
+          maxRange: 10000,
+          gridLines: 12
+        }
+      };
+    }
+  }
 }
